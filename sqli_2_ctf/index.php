59831c7bee903c6a045bf644853f050209f96207
amrzakaria <amrzakaria921@gmail.com>
2022-08-26 16:13:11 -0400

    sqli_2_added

---
+++ sqli_2_ctf/index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Form</title>
+    <link rel="stylesheet" href="style_.css"/>
+</head>
+<body>
+    <div class="two">
+        <h1>more harder challenge</h1>
+        <form action="test.php" method="POST">
+        <p>Username</p>
+        <input type="text" required placeholder="Username" name="user">
+        <p>Password</p>
+        <input type="password" required name="pass" placeholder="password">
+       <button>submit</button>
+       </form>
+       </div>
+</body>
+
+</html>
